7330ae6b6977c657adead3c74c884b1cd9adfbdb
Dhineshkumar <115341448+dk172923@users.noreply.github.com>
2024-10-20 13:28:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://learn-hub-frontend.vercel.app'
 }));
 app.use(express.json());
 
